8f1dcb0413bfd7f3a2f7d2937bb51410b44ba40c
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-06-23 12:39:14 -0500

    add 1st and 2nd property to constructor

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -1,9 +1,12 @@
 import Haiku from './../src/haiku';
 
 describe('Haiku', () => {
-  test("should recognize first sentence", () => {
+  test("should recognize first string", () => {
     const haiku = new Haiku("hello","good","bye")
     expect(haiku.firstString).toEqual("hello")
   })
-  
+  test("should recognize second string", () => {
+    const haiku = new Haiku("hello","good","bye")
+    expect(haiku.secondString).toEqual("good")
+  })
 })
